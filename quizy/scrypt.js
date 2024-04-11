7f9148889decd317d609dfd1e35179115317025e
x--kit <143827034+rahul-k23@users.noreply.github.com>
2024-04-11 12:18:00 +0530

    Initial commit

---
+++ quizy/scrypt.js
@@ -0,0 +1,499 @@
+const quizdb = [
+
+//    HTML 10 QUESTION ------>
+
+    {
+        question: "Q1: What does abbreviation HTML stand for",
+        a: "hallo to my land",
+        b: "hey text markup language",
+        c: "hypertext markup language",
+        d: "hyper markup language",
+        ans: "ans3"
+    },
+
+    {
+        question: "Q2: How many sizes of headers are available in HTML by default",
+
+        a: "5",
+        b: "1",
+        c: "3",
+        d: "6",
+        ans: "ans1"
+    },
+
+    {
+        question: "Q3: Which one of the following also known as Conditional Expression:",
+        a: "Alternative to if-else",
+        b: "Switch statement",
+        c: "If-then-else statement",
+        d: "immediate if ",
+        ans: "ans3"
+    },
+
+    {
+        question: "Q4: In JavaScript, what is a block of statement?",
+        a: "Conditional block",
+        b: "block that combines a number of statements into a single compound statement",
+        c: "both conditional block and a single statement",
+        d: "block that contains a single statement",
+        ans: "ans4"
+    },
+
+
+    {
+        question: "Q5: Which one of the  known as Conditional Expression:",
+        a: "Alternative to if-else",
+        b: "Switch statement",
+        c: "immediate if ",
+        d: "If-then-else statement",
+        ans: "ans1"
+    },
+
+
+    {
+        question: "Q6:Who is the father of HTML",
+        a:" Rasmus Lerdorf",
+        b: "Tim Berners-Lee",
+        c: "Brendan Eich",
+        d: "Sergey Brin",
+        ans:"ans2",
+      
+    },  
+  
+
+
+    {
+        question:"Q7. Which of the following is used to read an HTML page and render it",
+        a: "Web server",
+        b: "Web network",
+        c: "Web browser",
+        d: "Web matrix",
+        ans:"ans3"
+      
+    }, 
+
+    {
+        question:"Q8.Which of the following is not a difference between HTML and XHTML",
+        a: "Charset in both html and xhtml is “text/html",
+        b: "Tags and attributes are case-insensitive in HTML but not in XHTML",
+        c: "Special characters must be escaped using character entities in XHTML unlike HTML",
+        d: " Charset in html is “text/html” where as in xhtml it is “application/xml+xhtml",
+        ans:"ans1"
+      
+    },
+
+    {
+        question:"Q9.  Which of the following tag is used for inserting the largest heading in HTML",
+        a: "head",
+        b: "<h1>",
+        c: "<h6>",
+        d: "heading",
+        ans:"ans2"
+      
+    }, 
+     
+
+    {
+        question:"Q10.  What is DOM in HTML",
+        a: " Language dependent application programming",
+        b: "Hierarchy of objects in ASP.NET",
+        c: "Application programming interface",
+        d: " Convention for representing and interacting with objects in html documents",
+        ans:"ans4"
+      
+    }, 
+   
+// CSS 10 QUESTION----->
+
+
+
+
+      {
+
+        question:"Q11.What is CSS",
+        a:"CSS is a style sheet language ",
+        b: " CSS is designed to separate the presentation and content, including layout, colors, and fonts",
+        c: "CSS is the language used to style the HTML documents",
+        d: "All of the mentioned",
+        ans:"ans4"
+
+
+
+      },
+       
+
+
+      {
+
+        question:"Q12.Which of the following tag is used to embed css in html page",
+        a:"<css>",
+        b: " <!DOCTYPE html>",
+        c: "<script>",
+        d: "<style>",
+        ans:"ans4"
+
+
+
+      },
+
+
+
+
+
+
+      {
+
+        question:"Q13. Which of the following CSS selectors are used to specify a group of elements",
+        a:"tag",
+        b: " id",
+        c: "class",
+        d: "both class and tag",
+        ans:"ans3"
+
+
+
+      },
+
+
+
+
+      {
+
+        question:"Q14. Which of the following has introduced text, list, box, margin, border, color, and background properties",
+        a:"HTML",
+        b: "PHP ",
+        c: "CSS",
+        d: "Ajas",
+        ans:"ans3"
+
+
+
+      },
+
+
+
+
+     
+      {
+       question:"Q15. The CSS property used to control the element's font-size is ",
+        a:"text-style",
+        b: "text-size ",
+        c: "font-size",
+        d: "none of the above",
+        ans:"ans3"
+
+
+
+      }, 
+   
+
+
+
+      {
+        question:"Q16. The CSS property used to draw a line around the elements outside the border",
+         a:"border",
+         b: "outline",
+         c: "padding",
+         d: "line",
+         ans:"ans2"
+ 
+ 
+ 
+       }, 
+
+
+
+
+
+
+
+
+       {
+        question:"Q17.Which of the following is used to specify the subscript of text using CSS ",
+         a:"vertical-align:sub",
+         b: "vertical-align:super",
+         c: "vertical-align:subscript",
+         d: "none of the above",
+         ans:"ans1"
+ 
+ 
+ 
+       }, 
+
+
+
+
+       {
+        question:"Q18. The CSS property used to specify the transparency of an element is  ",
+         a:"opacity",
+         b: "filter ",
+         c: "visibility",
+         d: "overplay",
+         ans:"ans1"
+ 
+ 
+ 
+       }, 
+
+
+
+
+
+       {
+        question:"Q19. Are the negative values allowed in padding property",
+         a:"yes",
+         b: "No ",
+         c: "can't say",
+         d: "may be",
+         ans:"ans2"
+ 
+ 
+ 
+       }, 
+
+
+
+
+
+       {
+        question:"Q20. The CSS property used to make the text bold is - ",
+         a:"font-weight : bold",
+         b: "weight: bold",
+         c: "font: bold",
+         d: "style: bold",
+         ans:"ans3"
+ 
+ 
+ 
+       }, 
+
+
+       {
+        question:"Q21. Which of the following is the correct syntax to make the background-color of all paragraph elements to yellow",
+         a:"p {background-color : yellow;}",
+         b: "p {background-color : #yellow;}",
+         c: "all {background-color : yellow;}",
+         d: "all p {background-color : #yellow;}",
+         ans:"ans1"
+ 
+ 
+ 
+       }, 
+
+
+// JAVASCRIPT 10 QUESTION
+
+{
+    question:"Q22. Which type of JavaScript language is ___ ",
+     a:"object-oriented",
+     b: "object-based",
+     c: "Assembly-language",
+     d: "High-level",
+     ans:"ans2"
+
+
+
+   },
+
+
+
+   {
+    question:"Q23. Which one of the following also known as Conditional Expression:",
+     a:"Alternative to if-else",
+     b: "Switch statement",
+     c: "if-then-else statement",
+     d: "immediate-if",
+     ans:"ans4"
+
+
+
+   },
+
+
+
+
+
+   {
+    question:"Q24. When interpreter encounters an empty statements, what it will do: ",
+     a:"Shows warning",
+     b: "prompts to complete the statement",
+     c: "Throws an error",
+     d: "ignores the statements",
+     ans:"ans4"
+
+
+
+   },
+
+
+
+
+   {
+    question:"Q25. The function and  var are known as: ",
+     a:"keywords",
+     b: "data types",
+     c: "declaration stamenets",
+     d: "prototypes",
+     ans:"ans3"
+
+
+
+   },
+
+
+
+
+   {
+    question: "Q26. Which of the following type of a variable is volatile?" ,
+     a:"Mutable variable",
+     b: "Dynamic variable",
+     c: "Volatile Variable",
+     d: "Immutable Variable",
+     ans:"ans1"
+
+
+
+   },
+
+
+   {
+    question: "Q27. In the JavaScript, which one of the following is not considered as an error:" ,
+     a:"syntax error",
+     b: "Missing of semicolons",
+     c: "Division by zero",
+     d: "Missing of bracket",
+     ans:"ans3"
+
+
+
+   },
+
+
+
+
+
+   {
+    question: "Q28. Which of the following one is the property of the primary expression:" ,
+     a:"Contains only keywords",
+     b: "basic expressions containing all necessary functions",
+     c: "contains variable references alone",
+     d: "stand-alone expressions",
+     ans:"ans4"
+
+
+
+   },
+
+
+   {
+    question: "Q29. The new Point 3,2, is a kind of _______ expression" ,
+     a:"object creationm expression",
+     b: "primary function",
+     c: "invocation expression",
+     d: "coustomer calling expression",
+     ans:"ans1"
+
+
+
+   },
+
+
+
+
+   {
+    question: "Q30. Which one of the following is an ternary operator:" ,
+     a:"?",
+     b: ":",
+     c: "-",
+     d: "+",
+     ans:"ans1"
+
+
+
+   },
+
+
+
+ 
+];
+
+const questions = document.querySelector('.question');
+const option1 = document.querySelector('#option1');
+const option2 = document.querySelector('#option2');
+const option3 = document.querySelector('#option3');
+const option4 = document.querySelector('#option4');
+const submit = document.querySelector('#submit');
+
+const answers = document.querySelectorAll('.answer');
+
+const showScore = document.querySelector('#showscore');
+
+var questionCount = 0;
+var score = 0;
+
+function loadQuestion() {
+
+    const questionList = quizdb[questionCount];
+
+    questions.innerText = questionList.question;
+
+    option1.innerText =  questionList.a;
+    option2.innerText =  questionList.b;
+    option3.innerText =  questionList.c;
+    option4.innerText =  questionList.d;
+
+}
+
+loadQuestion();
+
+function getCheckedAnswer() {
+
+    var Answer;
+
+    answers.forEach((curAnsEle) =>{
+
+        if(curAnsEle.checked){
+
+            Answer = curAnsEle.id;
+        }
+    })
+
+    return Answer;
+};
+
+function deSellectAll() {
+
+    answers.forEach((curAnsEle) => curAnsEle.checked = false );
+
+};
+
+submit.addEventListener('click',() => {
+
+    const checkAnswer = getCheckedAnswer();
+    console.log(checkAnswer);
+
+    if(checkAnswer == quizdb[questionCount].ans ){
+
+        score++;
+
+    };
+
+    questionCount++; 
+    
+    deSellectAll();
+
+    if(questionCount < quizdb.length){
+
+        loadQuestion();
+
+    }else{
+
+        showScore.innerHTML = `
+
+        <h3> your score ${score}/${quizdb.length}</h3>
+        <button class="btn" onclick="location.reload()">Play Again</button>
+        `;
+        showScore.classList.remove('scorearea');
+    }
+});
+
